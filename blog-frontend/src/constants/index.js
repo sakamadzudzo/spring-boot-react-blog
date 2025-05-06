014d41b4ce99da9aaeadb7c13b0efb1ee8137060
Saka Shingirai Madzudzo <sakamadzudzo@gmail.com>
2025-05-06 11:10:19 +0200

    update API_BASE_URL

--- blog-frontend/src/constants/index.js
+++ blog-frontend/src/constants/index.js
@@ -1,4 +1,6 @@
-export const API_BASE_URL = 'http://localhost:8080';
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+// export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://spring-boot-react-blog-api.onrender.com';
+// export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = 'https://spring-boot-react-blog-api.onrender.com/oauth2/redirect'
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
